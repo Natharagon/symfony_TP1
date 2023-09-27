56fd4832fc6d6f98d3e2807fb5c13536935525e7
Nathan Ozimek <nathan.ozimek@etu.univ-lyon1.fr>
2023-09-27 14:05:17 +0200

    Affichage top 10 films + affiches

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,29 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Entity\Review;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use App\Entity\Image;
 
 #[Route('/movie')]
 class MovieController extends AbstractController
 {
     public function __construct(
-        private HttpClientInterface $client,
+        private readonly HttpClientInterface $tmdbClient,
     ) {}
 
     #[Route('/all')]
     public function getMovies(): Response
     {
-        $response = $this->client->request(
+        $response = $this->tmdbClient->request(
             'GET',
-            'https://api.themoviedb.org/3/movie/popular?api_key=c3262e5d343c3b0b8c7483a101a54078'
+            '/3/movie/popular'
         );
 
+        $apiMovies = $response->toArray()['results'];
+        $movies = [];
+        for ($i=0; $i<10; $i++){
+            $apiMovie = $apiMovies[$i];
+            $movie = new Movie();
+            $movie->setTitle($apiMovie['title']);
+            $movie->setLanguage($apiMovie['original_language']);
+            $image = new Image();
+            $image->setPath('https://image.tmdb.org/t/p/original' . $apiMovie['poster_path']);
+            $movie->addImage($image);
+            $movie->setReleaseDate(new \DateTime($apiMovie['release_date']));
+            $movie->setSynopsis(($apiMovie['overview']));
+            $movie->setIsAdult($apiMovie['adult']);
+            $review = new Review();
+            $review->setGrade($apiMovie['vote_average']);
+            $movie->addReview($review);
+            $movies[] = $movie;
+        }
+
         $content = $response->getContent();
-        $content = json_decode($content);
-        $content = json_encode($content->results);
-        return new Response($content);
+        $content = json_decode($content)->results;
+        
+        return $this->render('movie/popular.html.twig', [
+            'movies' => $movies,
+        ]);
+
+        
     }
 }
