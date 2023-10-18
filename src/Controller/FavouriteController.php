61f74cabcb07b39050e3628cfa5e1349ab951e12
Nathan Ozimek <nathozimek@gmail.com>
2023-10-18 15:39:46 +0200

    Affichage favourites

--- src/Controller/FavouriteController.php
+++ src/Controller/FavouriteController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Favourite;
 use App\Entity\Image;
+use App\Entity\Movie;
+use App\Entity\TV;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,21 +33,37 @@ class FavouriteController extends AbstractController
 
                 $apiMovie = json_decode($response->getContent());
                 $movie = new Movie();
-                $movie->setId($apiMovie['id']);
-                $movie->setTitle($apiMovie['title']);
+                $movie->setId($apiMovie->id);
+                $movie->setTitle($apiMovie->title);
                 $image = new Image();
-                $image->setPath('https://image.tmdb.org/t/p/original' . $apiMovie['poster_path']);
+                $image->setPath('https://image.tmdb.org/t/p/original' . $apiMovie->poster_path);
                 $movie->addImage($image);
-                $movie->setReleaseDate(new \DateTime($apiMovie['release_date']));
-                $movie->setGrade($apiMovie['vote_average']);
+                $movie->setReleaseDate(new \DateTime($apiMovie->release_date));
+                $movie->setGrade($apiMovie->vote_average);
+                $favourites[] = $movie;
             }
             else {
+                $response = $this->tmdbClient->request(
+                    'GET',
+                    '/3/tv/' . $favourite->getTvId()
+                );
 
+                $apiTv = json_decode($response->getContent());
+                $tv = new tv();
+                $tv->setId($apiTv->id);
+                $tv->setName($apiTv->name);
+                $image = new Image();
+                $image->setPath('https://image.tmdb.org/t/p/original' . $apiTv->poster_path);
+                $tv->addImage($image);
+                $tv->setStartDate(new \DateTime($apiTv->first_air_date));
+                $tv->setGrade($apiTv->vote_average);
+                $favourites[] = $tv;
             }
         }
 
+
         return $this->render('favourite/index.html.twig', [
-            'favouritesList' => $favouritesList,
+            'favourites' => $favourites,
         ]);
     }
 }
